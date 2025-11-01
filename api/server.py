ab798a452d0f25ac2eeb317fbec73e8e13048459
He Fei <droxer.he@gmail.com>
2025-11-01 15:30:00 +0800

    refactor: let hte cross origins be configurable.

--- api/server.py
+++ api/server.py
@@ -67,7 +67,7 @@ add_rate_limiting(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=config.cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
